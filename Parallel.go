e6f8e102dde8d5a17fd3cff9daa74109aff6a322
Eduard Urbach <e.urbach@gmail.com>
2017-11-02 07:12:02 +0100

    Added ParallelRepeat

--- Parallel.go
+++ Parallel.go
@@ -16,3 +16,20 @@ func Parallel(funcs ...func()) {
 
 	wg.Wait()
 }
+
+// ParallelRepeat starts the function asynchronously as goroutines n times and waits until they are completed.
+func ParallelRepeat(times int, funcs ...func()) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(funcs) * times)
+
+	for i := 0; i < times; i++ {
+		for _, fun := range funcs {
+			go func(task func()) {
+				task()
+				wg.Done()
+			}(fun)
+		}
+	}
+
+	wg.Wait()
+}
